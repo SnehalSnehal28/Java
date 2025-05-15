9ed5959f1547b1d36a0b98e05828f759cae39261
SnehalSnehal28 <snehalsolanki2003@gmail.com>
2025-05-15 12:39:24 +0530

    Add files via upload

---
+++ Java_test/Java_test/src/my_java/Square.java
@@ -0,0 +1,14 @@
+package my_java;
+
+public class Square {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=30;
+		int b=a*a;
+		System.out.println(a + " Square is "+ b);
+		
+
+	}
+
+}
